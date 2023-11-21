dc6e08456f01ac69462b555c61246162704dce94
fortnecc <christopher.fortner@sykes.com>
2023-11-21 10:51:34 -0600

    Multiple Device Updates

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Follow these steps to get started:
 
 1. Verify you are running python 3.9 or above ('python3 --version')
 2. Install requirements from 'requirements.txt' file ('python3 -m pip install -r requirements.txt')
-3. Run grab_routes.py with the required arguments of 'python3 grab_bgp_routes.py -u Username -d DeviceIP'
+3. Run grab_routes.py with the required arguments of 'python3 grab_bgp_routes.py -u Username -d IP(s)'
 4. Enter your password for device login when prompted
 5. Script will run and will output to screen and file the results
+
+Example for multiple IP's (devices):
+
+python3 grab_bgp_routes.py -u testuser -d 1.1.1.1 2.2.2.2 3.3.3.3
 
 It is recommended you use VSCode as an IDE and install the "flake8" and "Python" extensions.
 With VSCode you can directory open the Git Repository and setup a Python Virtual Environment
--- grab_bgp_routes.py
+++ grab_bgp_routes.py
@@ -8,52 +8,54 @@ from tabulate import tabulate
 
 
 class GetRoutes():
-    def __init__(self, device, username, password, verbose):
-        self.device = device
+    def __init__(self, devices, username, password, verbose):
+        self.devices = devices
         self.username = username
         self.password = password
         self.verbose = verbose
 
     def connectDevice(self):
-        remote_device = {
-            'device_type': 'autodetect',
-            'host': self.device,
-            'username': self.username,
-            'password': self.password,
-            'secret': self.password
-        }
+        for device in self.devices:
+            remote_device = {
+                'device_type': 'autodetect',
+                'host': device,
+                'username': self.username,
+                'password': self.password,
+                'secret': self.password
+            }
 
-        guesser = SSHDetect(**remote_device)
-        best_match = guesser.autodetect()
-        if verbose:
-            print(best_match)
-            print(guesser.potential_matches)
-        remote_device["device_type"] = best_match
+            guesser = SSHDetect(**remote_device)
+            best_match = guesser.autodetect()
+            if verbose:
+                print(best_match)
+                print(guesser.potential_matches)
+            remote_device["device_type"] = best_match
 
-        with ConnectHandler(**remote_device) as net_connect:
-            net_connect.enable()
-            prompt = net_connect.find_prompt()
-            prompt = prompt.replace('#', '')
-            net_connect.send_command('terminal length 0')
-            routes = net_connect.send_command('show ip bgp')
+            with ConnectHandler(**remote_device) as net_connect:
+                net_connect.enable()
+                prompt = net_connect.find_prompt()
+                prompt = prompt.replace('#', '')
+                net_connect.send_command('terminal length 0')
+                routes = net_connect.send_command('show ip bgp')
 
-            if 'cisco_ios' or 'cisco_xe' in best_match:
-                template = './templates/cisco_ios_show_ip_bgp.textfsm'
-            elif 'cisco_nxos' in best_match:
-                template = './templates/cisco_nxos_show_ip_bgp.textfsm'
-            output_file = prompt + '-results.csv'
+                if 'cisco_ios' or 'cisco_xe' in best_match:
+                    template = './templates/cisco_ios_show_ip_bgp.textfsm'
+                elif 'cisco_nxos' in best_match:
+                    template = './templates/cisco_nxos_show_ip_bgp.textfsm'
+                output_file = prompt + '-routes.csv'
 
-            with open(template) as f, open(output_file, 'w+') as output:
-                re_table = textfsm.TextFSM(f)
-                header = re_table.header
-                result = re_table.ParseText(routes)
-                if verbose:
-                    print(result)
-                    print(tabulate(result, headers=header))
-                output.write(tabulate(result, headers=header))
-                output.close()
+                with open(template) as f, open(output_file, 'w+') as output:
+                    re_table = textfsm.TextFSM(f)
+                    header = re_table.header
+                    result = re_table.ParseText(routes)
+                    if verbose:
+                        print(result)
+                        print(tabulate(result, headers=header))
+                    output.write(tabulate(result, headers=header))
+                    output.close()
 
-            net_connect.disconnect()
+                net_connect.disconnect()
+                print(prompt + ' completed successfully!')
 
 
 if __name__ == "__main__":
@@ -62,14 +64,14 @@ if __name__ == "__main__":
                                      description='This script will log into the device and grab the BP routes and parse them into a CSV file.',
                                      epilog='Created by Chris Fortner')
     parser.add_argument('-u', '--username', required=True)
-    parser.add_argument('-d', '--device', required=True)
+    parser.add_argument('-d', '--devices', nargs='+', default=[], required=True)
     parser.add_argument('-v', '--verbose', action='store_true', required=False)
 
     args = parser.parse_args()
 
     username = args.username
-    device = args.device
+    devices = args.devices
     verbose = args.verbose
     password = getpass()
-    run = GetRoutes(device, username, password, verbose)
+    run = GetRoutes(devices, username, password, verbose)
     run.connectDevice()
